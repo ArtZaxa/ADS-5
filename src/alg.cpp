8038d8084c356e789e280c78ca9658a8eabfaf3b
Lubitel <zaharushkinartem07@mail.ru>
2026-04-16 06:27:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -23,34 +23,28 @@ static void addToken(std::string& out, const std::string& token) {
     }
     out += token;
 }
-
 std::string infx2pstfx(const std::string& inf) {
     TStack<char, 100> ops;
     std::string out;
-
     for (std::size_t i = 0; i < inf.size();) {
         if (std::isspace(static_cast<unsigned char>(inf[i]))) {
             ++i;
             continue;
         }
-
         if (std::isdigit(static_cast<unsigned char>(inf[i]))) {
             std::size_t j = i;
             while (j < inf.size() && std::isdigit(static_cast<unsigned char>(inf[j]))) {
                 ++j;
             }
-
             addToken(out, inf.substr(i, j - i));
             i = j;
             continue;
         }
-
         if (inf[i] == '(') {
             ops.push(inf[i]);
             ++i;
             continue;
         }
-
         if (inf[i] == ')') {
             while (!ops.isEmpty() && ops.get() != '(') {
                 addToken(out, std::string(1, ops.get()));
@@ -62,7 +56,6 @@ std::string infx2pstfx(const std::string& inf) {
             ++i;
             continue;
         }
-
         if (isOp(inf[i])) {
             char cur = inf[i];
 
@@ -72,12 +65,10 @@ std::string infx2pstfx(const std::string& inf) {
                 addToken(out, std::string(1, ops.get()));
                 ops.pop();
             }
-
             ops.push(cur);
             ++i;
             continue;
         }
-
         ++i;
     }
 
@@ -87,19 +78,15 @@ std::string infx2pstfx(const std::string& inf) {
         }
         ops.pop();
     }
-
     return out;
 }
-
 int eval(const std::string& post) {
     TStack<int, 100> values;
-
     for (std::size_t i = 0; i < post.size();) {
         if (std::isspace(static_cast<unsigned char>(post[i]))) {
             ++i;
             continue;
         }
-
         if (std::isdigit(static_cast<unsigned char>(post[i]))) {
             int value = 0;
             while (i < post.size() && std::isdigit(static_cast<unsigned char>(post[i]))) {
@@ -109,16 +96,12 @@ int eval(const std::string& post) {
             values.push(value);
             continue;
         }
-
         if (isOp(post[i])) {
             int right = values.get();
             values.pop();
-
             int left = values.get();
             values.pop();
-
             int result = 0;
-
             if (post[i] == '+') {
                 result = left + right;
             } else if (post[i] == '-') {
@@ -128,12 +111,9 @@ int eval(const std::string& post) {
             } else if (post[i] == '/') {
                 result = left / right;
             }
-
             values.push(result);
         }
-
         ++i;
     }
-
     return values.get();
 }
