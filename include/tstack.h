f4b8f3b8b685e0b4d97bea07c451985a87300f28
Lubitel <zaharushkinartem07@mail.ru>
2026-04-16 06:47:04 +0300

    Update tstack.h spaces 3 times

--- include/tstack.h
+++ include/tstack.h
@@ -4,76 +4,75 @@
 
 template<typename T, int InitialSize>
 class TStack {
-    private:
-        T* arr;
-        int capacity;
-        int topIndex;
-        void resize(int newCapacity) {
-            if (newCapacity <= capacity) {
-                return;
-            }
-            T* temp = new T[newCapacity];
-            for (int i = 0; i <= topIndex; ++i) {
-                temp[i] = arr[i];
+ private:
+    T* arr;
+    int capacity;
+    int topIndex;
+    void resize(int newCapacity) {
+        if (newCapacity <= capacity) {
+            return;
+        }
+        T* temp = new T[newCapacity];
+        for (int i = 0; i <= topIndex; ++i) {
+            temp[i] = arr[i];
+        }
+        delete[] arr;
+        arr = temp;
+        capacity = newCapacity;
+    }
+ public:
+    TStack()
+        : capacity(InitialSize > 0 ? InitialSize : 1),
+          topIndex(-1) {
+        arr = new T[capacity];
+    }
+    TStack(const TStack& other)
+        : capacity(other.capacity),
+          topIndex(other.topIndex) {
+        arr = new T[capacity];
+        for (int i = 0; i <= topIndex; ++i) {
+            arr[i] = other.arr[i];
+        }
+    }
+    TStack& operator=(const TStack& other) {
+        if (this != &other) {
+            T* temp = new T[other.capacity];
+            for (int i = 0; i <= other.topIndex; ++i) {
+                temp[i] = other.arr[i];
             }
             delete[] arr;
             arr = temp;
-            capacity = newCapacity;
+            capacity = other.capacity;
+            topIndex = other.topIndex;
         }
-    public:
-        TStack()
-            : capacity(InitialSize > 0 ? InitialSize : 1),
-              topIndex(-1) {
-            arr = new T[capacity];
+        return *this;
+    }
+    ~TStack() {
+        delete[] arr;
+    }
+    bool isEmpty() const {
+        return topIndex == -1;
+    }
+    bool isFull() const {
+        return topIndex == capacity - 1;
+    }
+    T get() const {
+        if (isEmpty()) {
+            return T();
         }
-        TStack(const TStack& other)
-            : capacity(other.capacity),
-              topIndex(other.topIndex) {
-            arr = new T[capacity];
-            for (int i = 0; i <= topIndex; ++i) {
-                arr[i] = other.arr[i];
-            }
+        return arr[topIndex];
+    }
+    void pop() {
+        if (!isEmpty()) {
+            --topIndex;
         }
-        TStack& operator=(const TStack& other) {
-            if (this != &other) {
-                T* temp = new T[other.capacity];
-                for (int i = 0; i <= other.topIndex; ++i) {
-                    temp[i] = other.arr[i];
-                }
-                delete[] arr;
-                arr = temp;
-                capacity = other.capacity;
-                topIndex = other.topIndex;
-            }
-            return *this;
+    }
+    void push(const T& item) {
+        if (isFull()) {
+            resize(capacity * 2);
         }
-    
-        ~TStack() {
-            delete[] arr;
-        }
-        bool isEmpty() const {
-            return topIndex == -1;
-        }
-        bool isFull() const {
-            return topIndex == capacity - 1;
-        }
-        T get() const {
-            if (isEmpty()) {
-                return T();
-            }
-            return arr[topIndex];
-        }
-        void pop() {
-            if (!isEmpty()) {
-                --topIndex;
-            }
-        }
-        void push(const T& item) {
-            if (isFull()) {
-                resize(capacity * 2);
-            }
-            arr[++topIndex] = item;
-        }
+        arr[++topIndex] = item;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
